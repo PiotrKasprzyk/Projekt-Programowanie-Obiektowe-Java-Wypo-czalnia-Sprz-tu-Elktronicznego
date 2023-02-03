aa6dbf2fc252fb052a27c00f1384a1ddd4c4032e
PiotrKasprzyk <115540337+PiotrKasprzyk@users.noreply.github.com>
2023-02-03 11:57:35 +0100

    Add files via upload

---
+++ src/testyjednostkowe.java
@@ -0,0 +1,61 @@
+import org.testng.annotations.Test;
+
+public class testyjednostkowe {
+    private String login = "user";
+    private String password = "password";
+    private String email = "user@example.com";
+    private String address = "123 Main St.";
+    private String phone = "123-456-7890";
+
+    @Test
+    public void testyjednostkowe() {
+
+
+    }
+
+
+
+    @Test
+    public void testjednostkoweInvalidlogin() {
+
+    }
+
+    @Test
+    public void testjednostkoweInvalidpassword() {
+
+    }
+    @Test
+    public void testjednostkoweRegistration() {
+
+    }
+
+    @Test
+    public void testjednostkoweInvalidUsername() {
+
+    }
+
+    @Test
+    public void testjednostkoweInvalidPassword() {
+
+    }
+
+    @Test
+    public void testjednostkoweValidEmail() {
+
+    }
+
+    @Test
+    public void testjednostkoweInvalidEmail() {
+
+    }
+
+    @Test
+    public void testjednostkoweValidPhone() {
+
+    }
+
+    @Test
+    public void testjednostkoweInvalidPhone() {
+
+    }
+}
